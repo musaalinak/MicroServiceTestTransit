ccaf8f8b5d4bfc3e33ecf9d1a0bee400b334f41c
musas <musas@MSA>
2024-10-17 13:54:42 +0300

    Add project files.

---
+++ Messaging/Contracts/SendMessage.cs
@@ -0,0 +1,5 @@
+﻿namespace Messaging.Contracts
+{
+    public record SendMessage(string message);
+
+}
---
+++ Messaging/RabbitMqConsts/QueueTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.RabbitMqConsts
+{
+    public static class QueueTypes
+    {
+        public const string SendMessageQueue = "send-message-queue";
+    }
+}
